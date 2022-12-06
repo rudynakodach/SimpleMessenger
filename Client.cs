ceb3a3330de7c1962f99675913e19df17cfcc749
rudynakodach <whoppe7292@gmail.com>
2022-12-06 21:45:35 +0100

    Dodaj pliki projektów.

---
+++ Client.cs
@@ -0,0 +1,159 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+#pragma warning disable CS8602 //wyłuskanie odwołania, które może mieć wartośc 'null'.
+
+//dzień dobry tutaj ja ze wczoraj wieczorem
+
+//pisze ten kod bo najprawdopodobniej nie dostane sie na kolejny etap z konkursu kuratoryjnego z informatyki a zależy mi na dobrej ocenie na koneic roku :)
+
+//CLIENT-SIDE LAN MESSENGER
+
+// ~ rudynakodach (8D)
+namespace SimpleMessenger
+{
+	public static class Client
+	{
+		public static Socket _clientSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+		public const int serverPort = 100; //host only on port 100.
+
+		public static string username; //dont allow 'null' names
+
+		public static void Main()
+		{
+			Console.WriteLine("Mode: [HOST/CLIENT]");
+            Console.Write("> ");
+            string mode = Console.ReadLine().Trim();
+
+
+			if(mode == "HOST")
+			{
+				Server.ServerInit();
+				return;
+			}
+			else if (mode == "CLIENT")
+			{
+                Console.Title = "Client - SimpleMessenger";
+                while (string.IsNullOrWhiteSpace(username))
+                {
+                    Console.WriteLine("Username:");
+                    Console.Write("> ");
+                    username = Console.ReadLine().Trim();
+                }
+				ConnectLoop();
+				Thread thread = new Thread(ReceiveResponse);
+				thread.Start();
+				while (true)
+				{
+                    GetInput();
+                }
+            }
+		}
+
+		public static void GetInput()
+		{
+            Console.Write("> ");
+            string userInput = Console.ReadLine().Trim();
+
+
+            if (userInput.StartsWith("./"))
+			{
+				EnterCommand(userInput.Replace("./", null).Trim());
+			}
+			else
+			{
+				UploadEncryptedString(userInput);
+			}
+		}
+
+		/// <summary>
+		/// Usage of slash '/' commands.
+		/// </summary>
+		/// <param name="command">Command (without prefix)</param>
+		public static void EnterCommand(string command)
+		{
+			string initialCommand = command.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries)[0];
+			List<string> commandParams = command.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+			//todo
+			switch (initialCommand)
+			{
+				case "exit":
+					_clientSocket.Shutdown(SocketShutdown.Both); //dont crash the server when leacing PROPERLY
+					_clientSocket.Close(); //close the socket
+					Environment.Exit(0); //safely exit the app
+					break;
+
+				default:
+					Console.WriteLine("Unknown command.");
+					GetInput();
+					break;
+			}
+		}
+
+		/// <summary>
+		/// Attempt to connect to the server until succeeded.
+		/// </summary>
+		public static int connectionAttempts = 0;
+		public static void ConnectLoop()
+		{
+			while (!_clientSocket.Connected)
+			{
+				try
+				{
+					_clientSocket.Connect(IPAddress.Loopback, serverPort);
+				}
+				catch (SocketException ex)
+				{
+					connectionAttempts += 1;
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine(ex.Message);
+					Console.WriteLine($"Connection attempts: {connectionAttempts}\n");
+					Console.ResetColor();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Send an ASCII encrypted message in bytes to the server.
+		/// </summary>
+		/// <param name="text">Text to send and encode.</param>
+		public static void UploadEncryptedString(string text)
+		{
+			byte[] buffer = Encoding.ASCII.GetBytes(FormatSentMessage(text));
+			_clientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+		}
+
+		/// <summary>
+		/// Receive responses from the server.
+		/// </summary>
+		public static void ReceiveResponse()
+		{
+			while(true)
+			{
+                byte[] buffer = new byte[2048];
+                int received = _clientSocket.Receive(buffer, SocketFlags.None);
+
+                if (received == 0) { return; } //empty response; quit
+
+                byte[] data = new byte[received];
+                Array.Copy(buffer, data, received);
+                string text = Encoding.ASCII.GetString(data);
+                Console.WriteLine(text);
+
+				Thread.Sleep(100);
+            }
+		}
+
+		public static string FormatSentMessage(string text)
+		{
+			string message =
+				$"@u/{username}\t\t{DateTime.Now.ToLongTimeString()}\n" +
+                $"{text}\n";
+
+			return message;
+		}
+	}
+}
---
+++ Server.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleMessenger
+{
+    public static class Server
+    {
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 100;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+
+        public static string groupName;
+        public static string publicChatHistory = "";
+        public static void ServerInit()
+        {
+            Console.Title = $"Server {serverSocket.AddressFamily}";
+            groupName = Console.ReadLine().Trim();
+            SetupServer();
+            Console.ReadLine(); // When we press enter close everything
+            CloseAllSockets();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Server setup completed.");
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Close every connected socket
+        /// </summary>
+        private static void CloseAllSockets()
+        {
+            foreach (Socket socket in clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            serverSocket.Close();
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException)
+            {
+                return;
+            }
+
+            clientSockets.Add(socket);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            Console.WriteLine("Client connected, waiting for request...");
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client forcefully disconnected");
+
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine($"Received Text:\n{text}\nEND");
+
+            byte[] data = Encoding.ASCII.GetBytes(text);
+
+            foreach (var item in clientSockets)
+            {
+                try
+                {
+                    item.Send(data);
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+    }
+}
